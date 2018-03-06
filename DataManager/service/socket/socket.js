ed464d5b8dc7ee12f7d6e813451223ff03ea7893
kosyas <kosyas@PENNY-DEV>
2018-03-06 15:24:36 +0900

    penny

--- DataManager/service/socket/socket.js
+++ DataManager/service/socket/socket.js
@@ -1,37 +1,74 @@
+
+//config
 var cfg = require('../../conf/config.json');
-var app = require('http').createServer(handler)
-var io = require('socket.io').listen(app)
-var fs = require('fs')
+
+var app = require('express')();
+var server = require('http').createServer(app);
+
+// http server를 socket.io server로 upgrade한다
+var io = require('socket.io')(server);
 
 
-app.listen(cfg.listenPort);
+// serverip:port 으로 서버에 접속하면 클라이언트로 index.html을 전송한다
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-      if (err) {
-          res.writeHead(500);
-          return res.end('Error loading index.html');
-      }
+/**
+ * connection event handler
+ * connection이 수립되면 event handler function의 인자로 socket인 들어온다
+ */ 
+io.on('connection', function(socket) {
 
-      res.writeHead(200);
-      res.end(data);
+  // 접속한 클라이언트의 정보가 수신되면
+  socket.on('login', function(data) {
+    console.log('Client logged-in:\n name:' + data.name + '\n userid: ' + data.userid);
+
+    // socket에 클라이언트 정보를 저장한다
+    socket.name = data.name;
+    socket.userid = data.userid;
+
+    // 접속된 모든 클라이언트에게 메시지를 전송한다
+    io.emit('login', data.name );
   });
-}
 
-// socke.io를 활성화 하고 외부 클라이언트의 접속 을 기다린다.
-io.sockets.on('connection', function (socket) {
-  
-  // 접속한 대상에게 Data(Json 형태의 { hello: 'world'})를 전송한다.
-  socket.emit('news', { hello: 'world' });  
-  
-  // 명령 혹은 data를 받기 위해 대기한다. 
-  // 'my other event'라는 데이터를 받게되면 함수내용을 수행한다.
-  socket.on('my other event', function (data) {
-      console.log(data);
+  // 클라이언트로부터의 메시지가 수신되면
+  socket.on('chat', function(data) {
+    console.log('Message from %s: %s', socket.name, data.msg);
+
+    var msg = {
+      from: {
+        name: socket.name,
+        userid: socket.userid
+      },
+      msg: data.msg
+    };
+
+    // 메시지를 전송한 클라이언트를 제외한 모든 클라이언트에게 메시지를 전송한다
+    socket.broadcast.emit('chat', msg);
+
+    // 메시지를 전송한 클라이언트에게만 메시지를 전송한다
+    // socket.emit('s2c chat', msg);
+
+    // 접속된 모든 클라이언트에게 메시지를 전송한다
+    // io.emit('s2c chat', msg);
+
+    // 특정 클라이언트에게만 메시지를 전송한다
+    // io.to(id).emit('s2c chat', data);
+  });
+
+  // force client disconnect from server
+  socket.on('forceDisconnect', function() {
+    socket.disconnect();
+  })
+
+  socket.on('disconnect', function() {
+    console.log('user disconnected: ' + socket.name);
   });
 });
 
-
-
+server.listen(3000, function() {
+  console.log('Socket IO server listening on port 3000');
+});
+ 
 
