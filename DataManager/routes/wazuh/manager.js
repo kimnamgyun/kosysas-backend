92ba2fbbbb547497f59d3e2501ad12b13aa3739e
J <badcats01@gmail.com>
2018-03-20 14:19:41 +0900

    수정

--- DataManager/routes/wazuh/manager.js
+++ DataManager/routes/wazuh/manager.js
@@ -1,17 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-var wazuh = require('../../service/es/functions/wazuh.js');
+var wazuh = require('../service/es/functions/wazuh.js');
 
 var id = "foo";
 var pw = "bar";
 var host = "192.168.0.110:55000";
-
-/* use api test route */
-router.get('/test', function(req, res, next) {
-	
-	wazuh.get(id, pw, host, )
-});
 
 /* 
  * GET manager configuration 
