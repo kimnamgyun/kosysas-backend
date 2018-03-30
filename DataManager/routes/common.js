9ce76afcb19db11d4fa1b2f1a4386e361eb253af
J <badcats01@gmail.com>
2018-03-30 16:05:05 +0900

    304 error

---
+++ DataManager/routes/common.js
@@ -0,0 +1,15 @@
+/**
+ * 		common.js
+ * 		20180330 정종원
+ * 		routes 에서 사용할 공통 함수 작성을 위한 문서
+ */
+
+/*
+ * 		Header Setting for 304 Error
+ */
+module.exports.setHeader = function(res) {
+	
+	res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.header("Pragma", "no-cache");
+    res.header("Expires", 0)
+}
