3bcc4118fde6df8ebde6390b26fe47015103ea9a
J <badcats01@gmail.com>
2019-08-27 15:43:40 +0900

    //

--- DataManager/routes/auth/tokenManager.js
+++ DataManager/routes/auth/tokenManager.js
@@ -54,12 +54,12 @@ tokenManager.deleteToken = function() {
  */
 tokenManager.authToken = function(header) {
 	
+	console.log(header);
 	var bearerHeader = header["authorization"];	
 	if(typeof bearerHeader !== 'undefined') {
 		var bearer = bearerHeader.split(' ');
 		var token = bearer[1];
-		console.log(bearerHeader);
-		console.log(token);
+		//console.log(token);
 		try{
 			let decode = jwt.verify(token, "simon");
 			
